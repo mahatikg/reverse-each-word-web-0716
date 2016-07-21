c7437b20d95909aaf05048cd2bd6068a91cb14af
Mahati Gollamudi <mahatigollamudi@gmail.com>
2016-07-21 18:14:43 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(string)
+    arr = string.split(" ")
+    arr.each do |x|
+     x.reverse!
+    end
+    arr.join(" ")
+end
+
+
+def reverse_each_word(string)
+  arr = string.split(" ")
+    arr.collect do |x|
+     x.reverse!
+    end
+arr.join(" ")
+end
